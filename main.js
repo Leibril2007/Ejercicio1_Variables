aebf3ec696f2fbc4a5e64edd8662d9bc492f7eef
Leibril <ariabi.sr@gmail.com>
2025-01-14 11:37:58 -0600

    Ejercicio 1 variables

---
+++ main.js
@@ -0,0 +1,53 @@
+// VARIABLES
+
+let edad = 18;
+let nombre = "Ariel";
+let decimal = 1.5;
+
+const PI = 3.1416;
+
+// EJercicio
+let carrera = "V Bachillerato en Computación"
+let dispositivos_utilizados = "Computadoras";
+let dispositivo_proyeccion = "televisor";
+let cantidad_total_alumnos = 21;
+let cantidad_alumnas = 7;
+let cantidad_sillas = 22;
+let altura_promedio =  1.65;
+const cantidad_pizzarrones = 1;
+const cantidad_ventanas = 3;
+const cantidad_conectores= 4;
+
+
+//------------- EJERCICIO 1 -------------
+
+// ------ PROBLEMA #1
+
+// Datos en Sistema Internacional de Unidades
+
+let velocidad_inicial= 0;
+let velocidad_final = 30;
+let tiempo = 10;
+
+
+// ------ PROBLEMA #2
+
+let lote_productos_comprados = 150;
+let precio_de_cada_producto_comprado = 50;
+let precio_a_vender_por_unidad= 75;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
